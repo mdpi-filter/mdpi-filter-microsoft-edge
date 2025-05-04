42c3f7767ae799edb976e27df4bb03bdf7e7a89e
jnton <84038748+jnton@users.noreply.github.com>
2025-05-04 21:26:33 +0200

    Update domains.js

--- content/domains.js
+++ content/domains.js
@@ -1,21 +1,30 @@
 // content/domains.js
+// Define the four search‐site configurations
 window.MDPIFilterDomains = {
+
+  // Google Web
   googleWeb: {
     host: 'www.google.com',
     path: /^\/search/,
     container: 'div.g',
     linkSelector: 'a[href*="mdpi.com"]'
   },
+
+  // Google Scholar
   scholar: {
     host: 'scholar.google.com',
     container: 'div.gs_r',
     linkSelector: 'a[href*="mdpi.com"]'
   },
+
+  // PubMed (no outbound links, only DOI in text)
   pubmed: {
     host: 'pubmed.ncbi.nlm.nih.gov',
     itemSelector: 'article.full-docsum',
     doiPattern: '10.3390'
   },
+
+  // Europe PMC (matches any subdomain of europepmc.org)
   europepmc: {
     hostRegex: /europepmc\.org$/,
     itemSelector: 'li.separated-list-item',
